e8fe2851a187d38e09180ea03f9cb5d6ae47fb4c
Dev Aggarwal <devxpy@gmail.com>
2020-01-02 17:53:21 +0530

    add onSSIDChanged

--- lib/src/wifi_scanner_mixin.dart
+++ lib/src/wifi_scanner_mixin.dart
@@ -4,6 +4,8 @@ import 'package:wifi_connect/wifi_connect.dart';
 mixin WifiScannerMixin<T extends StatefulWidget> implements State<T> {
   var connectedSSID = '';
 
+  void onSSIDChanged(String ssid) {}
+
   Future<void> startWifiScanner({
     Duration period: const Duration(seconds: 1),
   }) async {
@@ -14,6 +16,7 @@ mixin WifiScannerMixin<T extends StatefulWidget> implements State<T> {
         setState(() {
           connectedSSID = newValue;
         });
+        onSSIDChanged(newValue);
       }
       await Future.delayed(period);
     }
